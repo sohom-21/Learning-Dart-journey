2c43fc87ad40a726420eb13d937f4d34181255f0
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-11-03 19:33:40 +0530

    This concludes the part of dart basics

--- lib/1.dart-basics/23.Expressions_and_statements_dart.dart
+++ lib/1.dart-basics/23.Expressions_and_statements_dart.dart
@@ -1,4 +1,12 @@
 void main() {
   int x = (10 + 3) % 4;
   print(x);
+  // up until now we have been talking about expression and statement.
+  //expressions hold a value at runtime.
+  // (10+3), (x +=10), [age > 16? 'adult' : 'child'] these have value, when assigned to a variable 
+  //Otherhand statement do not hold a value.
+  // print('next: ${value + 1}')
+  // int y  = x * 2;
+  // print('hello');
+  // have no more value further to assigned or modified.
 }
