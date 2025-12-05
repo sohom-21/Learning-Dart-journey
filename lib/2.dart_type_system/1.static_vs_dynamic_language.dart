3937ae1b91c716518b62cfcd2de3e485482ebfcf
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-12-05 12:53:46 +0530

    Improve comments on static vs dynamic languages example to clarify type restrictions

--- lib/2.dart_type_system/1.static_vs_dynamic_language.dart
+++ lib/2.dart_type_system/1.static_vs_dynamic_language.dart
@@ -1,6 +1,7 @@
 void main() {
   // lets start learning about static and dynamic languages
   String name = 'Andrea';
-  name = 10;
+  // name = 10;
+  // if you uncomment this line this will give you an error which tells that you cannot give other datatypes as input other than the defined type of the variable that is explicitly defined!
   print(name.toUpperCase());
 }
